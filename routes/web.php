633b5666cfa8448a65383c9a406aa32900db7605
John Marc Basbas <basbasjohnmarc@gmail.com>
2026-06-01 01:23:59 +0800

    fix redirect loop: root route now checks auth state

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,11 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => redirect()->route('login'));
+Route::get('/', function () {
+    return auth()->check()
+        ? redirect()->route('tasks.index')
+        : redirect()->route('login');
+});
 
 Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
